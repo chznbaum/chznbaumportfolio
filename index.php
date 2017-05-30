b34d777adbc71ca07329d70a58b37422d4413cc1
Chazona Baum <chznbaum@gmail.com>
2017-05-30 01:13:38 -0400

    Implemented the loop on index.php

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php get_header(); ?>
 
-<h1>Index File</h1>
+<section class="row">
+  <div class="small-12 columns text-center">
+    <div class="leader">
+
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+        <p><?php the_content(); ?></p>
+      
+      <?php endwhile; else : ?>
+      
+        <h1><?php _e( 'Nothing to See Here' ); ?></h1>
+        <p><?php _e( 'There doesn\'t seem to be any posts here.' ); ?></p>
+      
+      <?php endif; ?>
+
+    </div>
+  </div>
+</section>
 
 <?php get_footer(); ?>
